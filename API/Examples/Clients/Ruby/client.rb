658c740a1e61025cb864c94757dd4a2cdc44fb1a
iCLiKVAL <iclikval@riken.jp>
2015-08-13 16:28:01 +0900

    Create client.rb

---
+++ API/Examples/Clients/Ruby/client.rb
@@ -0,0 +1,23 @@
+require 'net/http'
+
+# iCLiKVAL API endpoint
+endpoint = 'http://10.64.206.171/api'
+resource = 'annotation'
+uri      = endpoint + '/' + resource
+url      = URI.parse(uri)
+
+request  = Net::HTTP::Get.new(url.to_s)
+request['User-Agent']    = 'MyApp/0.1'
+request['Accept']        = 'application/json'
+request['Authorization'] = 'Bearer xxxxxxxxxxxxxxxx' # Replace with real access token
+
+response = Net::HTTP.start(url.host, url.port) {|http|
+  http.request(request)
+}
+
+case response
+when Net::HTTPSuccess
+  puts response.body
+else
+  puts response.value
+end
