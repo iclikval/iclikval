6c0fd43b59143c99c063ed1ee8c73cb44a2ab9d0
iCLiKVAL <iclikval@riken.jp>
2015-08-13 17:01:56 +0900

    Update client.pl

--- API/Examples/Clients/Perl/client.pl
+++ API/Examples/Clients/Perl/client.pl
@@ -1,1 +1,33 @@
+use LWP::UserAgent;
 
+# iCLiKVAL API Endpoint
+my $endpoint = 'http://iclikval.riken.jp/api';
+# API Access Token
+my $token = 'xxxxxxxxxxxxxxxxxxxxxxxxx'; # Replace with real access token
+
+# Headers
+my $headers  = [
+    'Accept'        => 'application/json' ,
+    'Content-Type'  => 'application/json' ,
+    'Authorization' => 'Bearer '. $token
+];
+
+# Request
+my $method   = 'GET';
+my $resource = 'annotation';
+my $uri      = $endpoint .'/'. $resource;
+my $request  = HTTP::Request->new($method, $uri, $headers);
+
+# User agent
+my $browser  = LWP::UserAgent->new;
+my $response = $browser->request($request);
+
+
+# Response
+if ($response->is_success) {
+    print $response->content, "\n";
+} else {
+    print $response->status_line, "\n";
+    print $response->content, "\n";
+}
+~     
