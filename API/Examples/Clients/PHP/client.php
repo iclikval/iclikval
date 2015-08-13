ed9c2ca40dd0ed6bfbbae2e0641d2bef8b854362
iCLiKVAL <iclikval@riken.jp>
2015-08-13 18:28:42 +0900

    Update client.php

--- API/Examples/Clients/PHP/client.php
+++ API/Examples/Clients/PHP/client.php
@@ -13,15 +13,15 @@ $client = new GuzzleHttp\Client(['base_uri' => $endpoint]);
 
 try {
     $response = $client->get($resource, [
-    'headers' => [
-        'User-Agent'    => 'MyApp/1.0',
-        'Accept'        => 'application/json',
-        'Authorization' => 'Bearer ' . $token
-    ]]);
+        'headers' => [
+            'User-Agent'    => 'MyApp/1.0',
+            'Accept'        => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ]
+    ]);
 
     echo $response->getBody()->getContents();
 } catch (Exception $e) {
     echo $e->getMessage() . PHP_EOL;
 }
-
 ?>
